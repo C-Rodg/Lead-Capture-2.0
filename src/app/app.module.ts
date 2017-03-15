f4b1397b57182a118758d7648877672639b7b0c6
C-Rodg <rodgersbc@gmail.com>
2017-03-15 15:23:25 -0700

    Removed unnecessary pages/classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 
 import { Dashboard } from '../pages/dashboard/dashboard';
 import { List } from '../pages/list/list';
@@ -29,8 +27,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2,
     Dashboard,
     List,
     ScanCamera,
@@ -55,8 +51,6 @@ import { ReactiveFormsModule } from '@angular/forms';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2,
     Dashboard,
     List,
     ScanCamera,
