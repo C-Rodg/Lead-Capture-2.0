7662d7933ef7968a333097f384a475053d64334f
C-Rodg <rodgersbc@gmail.com>
2017-03-21 12:51:51 -0700

    Removed unused imports

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { List } from '../list/list';
